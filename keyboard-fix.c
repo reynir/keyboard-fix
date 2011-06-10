ce0d37c542d17ffc8474d084d074c0ef5b2414d9
Reynir <reynirr@gmail.com>
2011-06-10 01:18:22 -0700

    Added some arbitrary time limit
    A key stroke made a minute (or whatever) ago should not have any effect. I have not been able to compile it so I don't know wether it works or not.

--- keyboard-fix.c
+++ keyboard-fix.c
@@ -2,33 +2,39 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <sys/timeb.h>
 
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
+
+#define MAGIC_TIME_S 0
+#define MAGIC_TIME_MS 500
 
 void ListenForKeyboardEvents();
 void SendKeyPressEventFor(KeySym keysym, Display *display, Window *window);
 
 int main(int argc, char **argv) {
   pid_t pid = 0;
-	pid = fork();
+  pid = fork();
 
   if (pid == 0) {
     ListenForKeyboardEvents();
   } else {
     exit(0);
   }
-  
+
   return 0;
 }
 
 void ListenForKeyboardEvents() {
   unsigned int keycode;
   unsigned int revert;
-  
+  struct timeb tp;
+
   Window window;
   XEvent event;
   KeySym prev_char;
+  struct timeb prev_tp;
 
   Display *display = XOpenDisplay(NULL);
   Window root = XDefaultRootWindow(display); 
@@ -38,8 +44,22 @@ void ListenForKeyboardEvents() {
 
   while (true)  {
     XNextEvent(display, &event);
+    /* Read the time and do stuff */
+    ftime(&tp);
+
     if (event.type == KeyPress) {
       keycode = XLookupKeysym(&event.xkey, 0);
+
+      if (prev_tp == NULL)
+        continue; 
+      /* check to see if the two key strokes happened withing time limit */
+      if (tp.time - prev_tp.time > MAGIC_TIME_S ||
+          (tp.time - prev_tp.time = MAGIC_TIME_S &&
+           tp.millitm - prev_tp.millitm > MAGIC_TIME_MS)) {
+        prev_tp = tp;
+        prev_char = keycode;
+        continue;
+      }        
 
       if (prev_char == XK_a && keycode == XK_a) {
         SendKeyPressEventFor(XK_BackSpace, display, &window);
@@ -60,17 +80,18 @@ void ListenForKeyboardEvents() {
       XGetInputFocus(display, &window, &revert);
       XSelectInput(display, window, KeyPressMask | FocusChangeMask);
     }
+    prev_tp = tp;
   }
 }
 
 void SendKeyPressEventFor(KeySym keysym, Display *display, Window *window) {
   XKeyEvent event;
-  
+
   event.display = display;
   event.window = *window;
   event.type = KeyPress;
   event.keycode = XKeysymToKeycode(display, keysym);
-  
+
   XSendEvent(display, *window, True, KeyPressMask, (XEvent *) &event);
   XFlush(display);
 }
